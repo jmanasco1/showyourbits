366809b824a6e1f745dac28a481fd044d8408e7b
John <bigoldonk@hotmail.com>
2025-03-05 16:40:43 -0800

    Fix deploy script to use ESM format

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 // deploy.js - Script to help sync local environment with Vercel deployment
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 // Colors for console output
 const colors = {
